d22b7fcf1488648666f26e6164f2bee6f3d049e3
mert çobanoğlu <mertcbb@gmail.com>
2023-10-21 23:36:06 +0300

    Added repository pattern concretes

---
+++ src/Demergenza.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq.Expressions;
+using Demergenza.Application.Abstractions.Repositories;
+using Demergenza.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demergenza.Persistence.Repositories;
+
+public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity
+{
+    private readonly DbContext _context;
+    public DbSet<T> Table => _context.Set<T>();
+
+    public ReadRepository(DemergenzaDbContext dbContext)
+    {
+        _context = dbContext;
+    }
+
+    public IQueryable<T> GetAll()
+        => Table;
+
+    public IQueryable<T> GetWhere(Expression<Func<T, bool>> expression)
+        => Table.Where(expression);
+
+    public async Task<T> GetFirstAsync(Expression<Func<T, bool>> expression)
+        => await Table.FirstOrDefaultAsync(expression);
+    
+    public async Task<T> GetByIdAsync(string id)
+        => await Table.FirstOrDefaultAsync(t => t.Id == Guid.Parse(id));
+}
---
+++ src/Demergenza.Persistence/Repositories/WriteRepository.cs
@@ -0,0 +1,51 @@
+﻿using Demergenza.Application.Abstractions.Repositories;
+using Demergenza.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Demergenza.Persistence.Repositories;
+
+public class WriteRepository<T> : IWriteRepository<T> where T : BaseEntity
+{
+    private readonly DemergenzaDbContext _context;
+    public DbSet<T> Table { get; }
+
+    public WriteRepository(DemergenzaDbContext context)
+    {
+        _context = context;
+    }
+
+
+    public async Task<bool> AddAsync(T entity)
+    {
+        EntityEntry<T> entry = await _context.AddAsync(entity);
+        return entry.State == EntityState.Added;
+    }
+
+    public async Task<bool> AddAsync(List<T> entities)
+    {
+        await _context.AddRangeAsync();
+        return true;
+    }
+
+    public bool Remove(T entity)
+    {
+        var entry = Table.Remove(entity);
+        return entry.State == EntityState.Deleted;
+    }
+
+    public async Task<bool> Remove(string? id)
+    {
+        T entity = await Table.FirstOrDefaultAsync(entity => entity.Id == Guid.Parse(id));
+        return Remove(entity);
+    }
+
+    public bool Update(T entity)
+    {
+        EntityEntry<T> entry = _context.Update(entity);
+        return entry.State == EntityState.Modified;
+    }
+
+    public async Task<int> SaveAsync()
+        => await _context.SaveChangesAsync();
+}
